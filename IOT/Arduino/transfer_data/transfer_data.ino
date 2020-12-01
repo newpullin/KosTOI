133ead25404464ba78dc87ecb0f948d02011df64
Yongsoo Na <newpullin@naver.com>
2020-12-01 14:03:59 +0900

    라파로부터 on,off를 받아 LED 제어하기

--- IOT/Arduino/transfer_data/transfer_data.ino
+++ IOT/Arduino/transfer_data/transfer_data.ino
@@ -4,18 +4,26 @@
 #define TX_PIN 3
 #define RX_PIN 4
 #define DHT11_PIN 2
+#define GREEN_PIN 8
+#define YELLOW_PIN 7
 
 dht DHT;
 float hum;
 float temp;
 String s_hum = ":h:";
 String s_tem = ":t:";
+String readString;
+
 SoftwareSerial BT(TX_PIN, RX_PIN);
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   BT.begin(9600);
+  Serial.write("why");
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
@@ -25,6 +33,24 @@ void loop() {
   temp = DHT.temperature;
 
   BT.print(s_hum+String(hum)+s_tem+String(temp));
-  
-  delay(3000);
+  while(BT.available()) {
+    delay(3);
+    char c = BT.read();
+    readString += c;
+  }
+  readString.trim();
+
+  if(readString.length() > 0) {
+    if(readString.startsWith("on")) {
+      digitalWrite(GREEN_PIN, HIGH);
+      digitalWrite(YELLOW_PIN, LOW);
+    }
+    if(readString.startsWith("of")) {
+      digitalWrite(GREEN_PIN, LOW);
+      digitalWrite(YELLOW_PIN, HIGH);
+    }
+  }
+  Serial.println(readString);
+  readString = "";
+  delay(5000);
 }
